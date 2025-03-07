004ae00404dfed47f0870a91c94d50e854e3f244
boyadik69 <4Zhiraphael@gmail.com>
2025-03-07 09:13:58 +0800

    Added models for Ingredient, Recipe, and RecipeIngredient in models.py

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,16 @@
+"""This file contains the models for the recipebook."""
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    """Model for a single ingredient."""
+    name = models.CharField(max_length = 255)
+
+class Recipe(models.Model):
+    """Model for a recipe."""
+    name = models.CharField(max_length = 255)
+
+class RecipeIngredient(models.Model):
+    """Model for an ingredient in a recipe."""
+    quantity = models.CharField(max_length = 255)
+    ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)

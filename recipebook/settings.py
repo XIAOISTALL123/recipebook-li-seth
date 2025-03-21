d1d67ac9fa6d065d4e014b43d94a545786012a0a
boyadik69 <4Zhiraphael@gmail.com>
2025-03-21 19:30:36 +0800

    debugged base.html, edited recipe.html to extend base, added login urls to settings.py

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -70,6 +70,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'recipebook.wsgi.application'
 
+LOGIN_URL = 'accounts/login/'
+LOGIN_REDIRECT_URL = '/recipes/list/'
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases

9f16f2883444ce7ecb792f344a5842e6c61f2fe8
boyadik69 <4Zhiraphael@gmail.com>
2025-03-21 12:20:23 +0800

    altered RecipeDetailView to require login

--- ledger/views.py
+++ ledger/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Ingredient, Recipe, RecipeIngredient
 
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipeList.html' 
     
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe.html' 
+    redirect_field_name = ''
 
 

03caef75793a944e34b2b856392cd121485401a3
boyadik69 <4Zhiraphael@gmail.com>
2025-04-10 09:44:42 +0800

    added functionality for users to add recipes

--- ledger/forms.py
+++ ledger/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Recipe
+from .models import Recipe, RecipeIngredient
+from django.forms import inlineformset_factory
 
 class RecipeCreateForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = '__all__'
+
+IngredientFormSet = inlineformset_factory(
+    Recipe, RecipeIngredient,
+    fields = '__all__',
+    extra = 1, can_delete = False
+)
--- ledger/views.py
+++ ledger/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Ingredient, Recipe, RecipeIngredient
-from .forms import RecipeCreateForm
+from .forms import RecipeCreateForm, IngredientFormSet
 
 class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
@@ -23,3 +23,24 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
     form_class = RecipeCreateForm
     template_name = 'recipeCreate.html'
     redirect_field_name = 'accounts/login'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = IngredientFormSet(self.request.POST)
+        else:
+            context['formset'] = IngredientFormSet()
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        if formset.is_valid():
+            self.object = form.save()
+            ingredients = formset.save(commit = False)
+            for ingredient in ingredients:
+                ingredient.recipe = self.object
+                ingredient.save()
+            return redirect(self.get_success_url())
+        else:
+            return self.form_invalid(form)
